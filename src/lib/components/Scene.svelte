bcfd4384d7347b3aa3c480244af20874dc71b68a
Sergej Moor <sergejmoor01@gmail.com>
2024-11-09 23:44:34 +0100

    added ISS model

--- src/lib/components/Scene.svelte
+++ src/lib/components/Scene.svelte
@@ -2,6 +2,8 @@
 	import { T } from '@threlte/core';
 	import { ContactShadows, Float, Grid, OrbitControls } from '@threlte/extras';
 	import { TextureLoader } from 'three';
+	import { useGltf } from '@threlte/extras';
+	import { Group } from 'three';
 
 	const textureLoader = new TextureLoader();
 	const earthTexture = textureLoader.load('/2k_earth_daymap.jpg');
@@ -10,20 +12,20 @@
 	const cloudsTexture = textureLoader.load('/2k_earth_clouds.jpg');
 </script>
 
-<T.PerspectiveCamera makeDefault position={[-10, 10, 10]} fov={15}>
+<T.PerspectiveCamera makeDefault position={[-10, 10, 10]} fov={45}>
 	<OrbitControls
 		autoRotate
 		enableZoom={true}
 		minDistance={5}
-		maxDistance={20}
+		maxDistance={50}
 		enableDamping
-		autoRotateSpeed={0.5}
+		autoRotateSpeed={0.2}
 		target.y={1.5}
 	/>
 </T.PerspectiveCamera>
 
 <T.AmbientLight intensity={0.8} />
-
+<!-- 
 <Grid
 	position.y={-0.001}
 	cellColor="#ffffff"
@@ -31,7 +33,7 @@
 	sectionThickness={0}
 	fadeDistance={25}
 	cellSize={2}
-/>
+/> -->
 
 <ContactShadows scale={10} blur={2} far={2.5} opacity={0.5} />
 
@@ -52,6 +54,13 @@
 	<!-- Cloud Layer -->
 	<T.Mesh>
 		<T.SphereGeometry args={[2.05, 64, 64]} />
-		<T.MeshPhongMaterial map={cloudsTexture} transparent={true} opacity={0.2} depthWrite={false} />
+		<T.MeshPhongMaterial map={cloudsTexture} transparent={true} opacity={0.8} depthWrite={false} />
 	</T.Mesh>
+
+	<!-- ISS -->
+	<T.Group position.x={1.5} position.y={1.5} position.z={1.5}>
+		{#await useGltf('/ISS_stationary.glb') then gltf}
+			<T scale={0.005} is={gltf.scene} />
+		{/await}
+	</T.Group>
 </Float>
